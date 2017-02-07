589c72136176a1a2ce7480a6517e95e3b6ba6f9a
Robin Yonge <robinfarrowyonge@gmail.com>
2017-02-07 13:08:02 +0000

    add solution for ex3

---
+++ lab4/apiexample3.py
@@ -0,0 +1,12 @@
+import datetime
+import requests
+
+Exchange_Rate=[]
+
+for i in range(30):
+	my_date = datetime.date.today() + datetime.timedelta(days=-i)
+	my_query_url = 'https://api.fixer.io/'+'{}?symbols={}&base={}'.format(my_date, 'USD,GBP,', "GBP")
+	my_response = requests.get(my_query_url).json()
+	Exchange_Rate.append(my_response)
+
+print(
