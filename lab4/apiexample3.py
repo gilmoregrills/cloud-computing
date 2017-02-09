0417cea88eb93a24d0cd33cc3253596f89375723
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-09 10:25:07 +0000

    finishw lab4, except bonus

--- lab4/apiexample3.py
+++ lab4/apiexample3.py
@@ -5,8 +5,9 @@ Exchange_Rate=[]
 
 for i in range(30):
 	my_date = datetime.date.today() + datetime.timedelta(days=-i)
-	my_query_url = 'https://api.fixer.io/'+'{}?symbols={}&base={}'.format(my_date, 'USD,GBP,', "GBP")
-	my_response = requests.get(my_query_url).json()
+	my_query_url = 'https://api.fixer.io/'+'{}?symbols={}&base={}'.format(my_date, 'USD,GBP,', "GBP")	
+        my_response = requests.get(my_query_url).json()
 	Exchange_Rate.append(my_response)
+        
 
-print(
+print('Average exchange rate over the past 30 days is: {}'.format(Exchange_Rate)
