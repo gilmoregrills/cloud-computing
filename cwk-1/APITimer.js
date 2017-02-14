4b0e7c1fb50a055f2991200e1909f81540ee4742
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-14 22:06:10 +0000

    still bit broken, need to add callbacks

--- cwk-1/APITimer.js
+++ cwk-1/APITimer.js
@@ -3,14 +3,21 @@ const request = require('request');
 var results;
 var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=129rectorylanelondon';
 var count = 1;
-var averageResponse;
-var medianResponse;
-var maximumResponse;
-var standardDeviation;
+var totalResponse = 0;
+var averageResponse = calcAverage();
+var medianResponse = calcMedian();
+var maximumResponse = calcMax();
+var standardDeviation = calcSD();
 
 APItimer(url, count);
-processData(totalResponse, results, count);
 
+//at the moment this prints before the results of the function below, need to use callbacks to execute when APItimer completes
+function printResults() {
+	console.log('The results are in!\nThe average response time was: '+averageResponse+'\nThe median response time was: '+medianResponse+'\nThe max response times was: '+maximumResponse+'\nThe standard deviation was: '+standardDeviation);
+};
+
+//takes a url string, and the number of times you want to make the request
+//then populates the results table while keeping track of the total response time
 function APItimer(url, calls) {
 	results = new Array(calls, 2);
 	//make the API calls, store returned data/response times in array
@@ -25,12 +32,21 @@ function APItimer(url, calls) {
 			totalResponse += results[i,1];
 		});
 	}
+	printResults();
 };
 
-function processData (indexes, array, total) {
-	averageResponse = total/indexes;
-	medianResponse = 
-	maximumResponse = 
-	standardDeviation =
+function calcAverage() {
+	totalResponse/count;
 };
 
+function calcMedian() {
+
+};
+
+function calcMax() {
+
+};
+
+function calcSD() {
+
+};
