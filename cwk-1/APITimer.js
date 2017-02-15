2e0c2ae03172b9d0c486f52e6d0d5987b0d8b5e8
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-15 22:03:46 +0000

    add broken solution, add working but boring solution

--- cwk-1/APITimer.js
+++ cwk-1/APITimer.js
@@ -1,52 +1,58 @@
-const request = require('request');
+const request = require('sync-request');
 
-var results;
 var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=129rectorylanelondon';
 var count = 1;
+var results = [];
 var totalResponse = 0;
-var averageResponse = calcAverage();
-var medianResponse = calcMedian();
-var maximumResponse = calcMax();
-var standardDeviation = calcSD();
+		
+testAPI(url, count);
+printResults();
 
-APItimer(url, count);
 
-//at the moment this prints before the results of the function below, need to use callbacks to execute when APItimer completes
 function printResults() {
-	console.log('The results are in!\nThe average response time was: '+averageResponse+'\nThe median response time was: '+medianResponse+'\nThe max response times was: '+maximumResponse+'\nThe standard deviation was: '+standardDeviation);
+	console.log('The results are in!\nThe average response time was: '+calcAverage()+'\nThe median response time was: '+calcMedian()+'\nThe max response times was: '+calcMax()+'\nThe standard deviation was: '+calcSD());
 };
 
-//takes a url string, and the number of times you want to make the request
-//then populates the results table while keeping track of the total response time
-function APItimer(url, calls) {
-	results = new Array(calls, 2);
-	//make the API calls, store returned data/response times in array
-	for (var i = 0; i <= calls; i++) {
-		var startTime = Date.now();
-		request(url, function (error, response, data) {
-			var endTime = Date.now();
-			if (error) throw (error);
-			results[i,0] = data;
-			results[i,1] = endTime - startTime;
-			console.log(results[i,0], '\nresponsetime: ', results[i,1]);
-			totalResponse += results[i,1];
-		});
+function testAPI (url, calls) {
+	for (var i = 0; i < calls; i++) {
+		apiTimer(url);
 	}
-	printResults();
+};
+
+function apiTimer (url, index) {
+		var startTime = Date.now();
+		var result = request('GET', url);
+		var responsetime = Date.now() - startTime;
+	//	console.log(responsetime);	
+		results[results.length-1] = responsetime;
+	//	console.log(results[results.length-1]);
+		totalResponse += responsetime;
 };
 
 function calcAverage() {
-	totalResponse/count;
+	return totalResponse/count;
 };
 
 function calcMedian() {
-
+	if count is even
+		return mean of two middle
+	if count is odd
+		return middle number
 };
 
 function calcMax() {
-
+	var tmp = 0;
+	for (time in results) {
+		if(time > tmp) {
+			tmp = time;
+		}
+	}
+	return tmp;
 };
 
 function calcSD() {
-
+	var mean = calcAverage();
+	for (time in results) {
+		see how much they deviate from average??
+	}
 };
