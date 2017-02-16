f1543bb25fd2fd4cebe39f7776a2758e5722599c
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-16 10:22:49 +0000

    beautify APITimer.js, fool around with some map/reduce functions for no real reason

--- cwk-1/APITimer.js
+++ cwk-1/APITimer.js
@@ -1,50 +1,44 @@
 const request = require('sync-request');
 
-var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=129rectorylanelondon';
-var count = 1;
-var results = [];
-var totalResponse = 0;
-		
-testAPI(url, count);
-printResults();
-
-
-function printResults() {
-	console.log('The results are in!\nThe average response time was: '+calcAverage()+'\nThe median response time was: '+calcMedian()+'\nThe max response times was: '+calcMax()+'\nThe standard deviation was: '+calcSD());
-};
-
-function testAPI (url, calls) {
-	for (var i = 0; i < calls; i++) {
-		apiTimer(url);
-	}
-};
-
-function apiTimer (url, index) {
+var url = 'http://pokeapi.co/api/v2/pokemon/145/'; //the API to GET from
+var calls = 10; //the number of times you want to GET
+var count = new Array(calls).fill(0);
+	
+//creates array named results containing the response time of each request
+var results = count.map(function() {
 		var startTime = Date.now();
-		var result = request('GET', url);
+		var result = request('GET', url);//function may change if uni machines lack node or if it proves too slow
 		var responsetime = Date.now() - startTime;
-	//	console.log(responsetime);	
-		results[results.length-1] = responsetime;
-	//	console.log(results[results.length-1]);
-		totalResponse += responsetime;
-};
+		return responsetime;
+});
+
+//for debugging, prints response times array/averages etc
+//it's only an IIFE because it used to have other statements in
+(function() {
+	console.log('Response Times: '+results.toString());
+	console.log('Calculations Complete!\nThe average response time was: '+calcAverage()+'\nThe median response time was: '+calcMedian()+'\nThe max response times was: '+calcMax()+'\nThe standard deviation was: '+calcSD());
+}());
 
 function calcAverage() {
-	return totalResponse/count;
+	var total = results.reduce(function(total, value) {
+		return total + value;
+	}, 0);
+	return total/results.length;
 };
 
 function calcMedian() {
-	if count is even
-		return mean of two middle
-	if count is odd
-		return middle number
+	if(results.length%2 == 0) {
+		return ((results[(results.length/2)] + results[(results.length/2)+1]) /2);
+	} else {
+		return (results[Math.round(results.length/2)-1]);
+	}
 };
 
 function calcMax() {
 	var tmp = 0;
-	for (time in results) {
-		if(time > tmp) {
-			tmp = time;
+	for (var i = 0; i < results.length; i++) {
+		if(results[i] > tmp) {
+			tmp = results[i];
 		}
 	}
 	return tmp;
@@ -52,7 +46,14 @@ function calcMax() {
 
 function calcSD() {
 	var mean = calcAverage();
-	for (time in results) {
-		see how much they deviate from average??
-	}
+	var squareVariance = results.map(function(result){
+		var index = result - mean;
+		var squareIndex = index * index;
+		return squareIndex;
+	});
+	var total = squareVariance.reduce(function(total, value) {
+		return total + value;
+	}, 0);
+	sd = Math.sqrt(total/results.length);
+	return sd;	
 };
--- cwk-1/brokenAPItimer.js
+++ cwk-1/brokenAPItimer.js
@@ -2,11 +2,12 @@ const request = require('request');
 
 var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=129rectorylanelondon';
 var count = 1;
-var results = [];
 var totalResponse = 0;
-var waiting = 0;
 
-testAPI(url, count);
+var results = new Array(count);
+
+results.map(apiTimer(url, count));
+
 printResults();
 //RIGHT NOW THE PROBLEM IS THAT PRINT RESULTS IS CALLED BEFORE THERE
 //ARE RESULTS TO PRINT
@@ -15,25 +16,17 @@ function printResults() {
 	console.log('The results are in!');
 };
 
-//makes a call to the specified url, a specified number of times
-function testAPI (url, calls) {
-	for (var i = 0; i < calls; i++) {
-		apiTimer(url, calls);
-	}
-};
-
 //checks the current time, makes the API call
 //on the callback it checks the time and calculates the response time from that
 //adds responsetime to results array
 function apiTimer (url, index) {
-		waiting = 1;
+		var tmp;
 		var startTime = Date.now();
 		request(url, function (error, response, data) {
 			var responsetime = Date.now() - startTime;
 			if (error) throw (error);
 			console.log(data, '\nresponsetime: ', responsetime);
-			results[results.length-1] = responsetime;
-			waiting = 0;
+			v
 		});
 };
 
