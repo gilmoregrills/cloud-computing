fbdbf5f82010f9df2bbd22bbaf1da46970af3d76
gilmoregrills <robinfarrowyonge@gmail.com>
2017-02-16 18:04:43 +0000

    update apiTimer to comply with the spec, update README to reflect changes

--- cwk-1/README.md
+++ cwk-1/README.md
@@ -3,3 +3,7 @@
 First, enter the "npm install" command to install any dependencies. 
 
 Then just enter "node apiTimer.js" to execute. 
+
+To change the API url, rate limit, or number of requests, just modify the variables at the top of the file. 
+
+Anything else should be explained by comments/in the report. 
--- cwk-1/apiTimer.js
+++ cwk-1/apiTimer.js
@@ -1,17 +1,33 @@
 const request = require('syncrequest');
 
 var url = 'http://www.pokeapi.co/api/v2/pokemon/zapdos/'; //the API to GET from
-var calls = 10; //the number of times you want to GET
-var count = new Array(calls).fill(0);
+var rateLimit = 300;
+var requests = 7; //the number of times you want to GET
 
-//returns array named results containing the response time of each request
-var results = count.map(function() {
+
+//takes as input the specified url, number of requests you want made, plus limit
+//main conditional checks whether you want to test 1 response or >1
+//if many, checks if requests > limit, if yes sets requests to limit-1
+//returns array of results if many, single int if not
+function testAPI (url, requests, limit) {
+	if (requests > 1) {
+		requests = requests>limit ? limit-1 : requests;
+		var count = new Array(requests).fill(0);
+		var results = count.map(function() {
+			var startTime = Date.now();
+			var result = request.sync(url);
+			var responsetime = Date.now() - startTime;
+			//for debugging: console.log(JSON.parse(result.body).forms[0]);
+			return responsetime;
+		});
+		return results;
+	} else {
 		var startTime = Date.now();
 		var result = request.sync(url);
 		var responsetime = Date.now() - startTime;
-		//for debugging: console.log(JSON.parse(result.body).forms[0]);
 		return responsetime;
-});
+	}
+};
 
 /*
 asynchronous (broken) alternative
@@ -25,48 +41,55 @@ var results = count.map(function (url) {
 });
 */
 
-//for debugging, prints response times array/averages etc
-//it's only an IIFE because it used to have other statements in
+//IIFE saves one line of function call!
+//potentially redundant conditional, if 1 just prints the response time
+//if calls >1 gets array of response times and prints averages etc
 (function() {
-	console.log('Response Times: '+results.toString());
-	console.log('Calculations Complete!\nThe average response time was: '+calcAverage()+'\nThe median response time was: '+calcMedian()+'\nThe max response times was: '+calcMax()+'\nThe standard deviation was: '+calcSD());
+	if (requests == 1) {
+		console.log(testAPI(url, requests, rateLimit));
+	} else {
+		var results = testAPI(url, requests, rateLimit);
+		console.log('Response Times: '+results.toString());
+		console.log('Calculations Complete!\nThe average response time was: '+calcAverage(results)+'\nThe median response time was: '+calcMedian(results)+'\nThe max response times was: '+calcMax(results)+'\nThe standard deviation was: '+calcSD(results));
+	}
 }());
 
-function calcAverage() {
-	var total = results.reduce(function(total, value) {
+function calcAverage(arr) {
+	var total = arr.reduce(function(total, value) {
 		return total + value;
 	}, 0);
-	return total/results.length;
+	return total/arr.length;
 };
 
-function calcMedian() {
-	if(results.length%2 == 0) {
-		return ((results[(results.length/2)] + results[(results.length/2)+1]) /2);
+function calcMedian(arr) {
+	if(arr.length%2 == 0) {
+		return ((arr[(arr.length/2)] + arr[(arr.length/2)+1]) /2);
 	} else {
-		return (results[Math.round(results.length/2)-1]);
+		return (arr[Math.round(arr.length/2)-1]);
 	}
 };
 
-function calcMax() {
+//replace loop w/ filter potentially
+function calcMax(arr) {
 	var tmp = 0;
-	for (var i = 0; i < results.length; i++) {
-		if(results[i] > tmp) {
-			tmp = results[i];
+	for (var i = 0; i < arr.length; i++) {
+		if(arr[i] > tmp) {
+			tmp = arr[i];
 		}
 	}
 	return tmp;
 };
 
-function calcSD() {
-	var mean = calcAverage();
-	var squareVariance = results.map(function(result){
-		var index = result - mean;
-		var squareIndex = index * index;
+function calcSD(arr) {
+	var mean = calcAverage(arr);
+	var squareVariance = arr.map(function(time){
+		var index = arr - mean;
+		var squareIndex = time * time;
 		return squareIndex;
 	});
 	var total = squareVariance.reduce(function(total, value) {
 		return total + value;
 	}, 0);
-	sd = Math.sqrt(total/results.length);
+	sd = Math.sqrt(total/squareVariance.length);
 	return sd;	
 };
