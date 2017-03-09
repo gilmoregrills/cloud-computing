3f1ae5827b51db3e7b55b43e80541567ebb25f31
gilmoregrills <robinfarrowyonge@gmail.com>
2017-03-09 11:47:04 +0000

    add flask app files (need completing)

--- lab6/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, validators
-
-class MessageForm(Form):
-   message = TextField(u'What is on your mind?', [validators.optional(), validators.length(max=200)])
-
-
-
